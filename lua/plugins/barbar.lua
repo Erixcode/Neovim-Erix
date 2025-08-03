d81472d45450cef1f14da51777d820fc5ba0e863
ErixPloit <35405767+Erixcode@users.noreply.github.com>
2025-08-03 16:02:02 +0330

    Add files via upload

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -7,9 +7,15 @@ return {
     init = function() vim.g.barbar_auto_setup = false end,
     opts = {
       -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
+       animation = true,
       -- insert_at_start = true,
-      -- …etc.
+       icons = {
+       -- Configure the icons on the bufferline when modified or pinned.
+        -- Supports all the base icon options.
+        modified = {button = '●'},
+        inactive = {button = '×'},
+        pinned = {button = '', filename = true},
+      },
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
