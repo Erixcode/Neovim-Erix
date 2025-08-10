80fc84b87c6bab6163dd78055594500f6fbf9e26
ErixPloit <35405767+Erixcode@users.noreply.github.com>
2025-08-10 13:01:21 +0330

    Update fzflua.lua

--- lua/plugins/fzflua.lua
+++ lua/plugins/fzflua.lua
@@ -6,8 +6,8 @@ return {
   dependencies = { "echasnovski/mini.icons" },
   opts = {},
   keys = {
-       { 
-            "<leader>ff", 
+       {
+            "<leader>ff",
             function() require('fzf-lua').files() end,
             desc="Find Files in Project Directory"
     },
@@ -16,30 +16,35 @@ return {
             function() require('fzf-lua').live_grep() end,
             desc="Find by Grepping Project Directory"
     },
-    { 
-            "<leader>fc", 
+    {
+            "<leader>fc",
             function() require('fzf-lua').files({cwd=vim.fn.stdpath("config")}) end,
             desc="Find Files in Current Working Directory"
     },
-    { 
-            "<leader>fb", 
+    {
+            "<leader>fb",
             function() require('fzf-lua').builtin() end,
             desc="Displays Every Builtin Fuzzy-Finder"
     },
-    { 
-            "<leader>fr", 
+    {
+            "<leader>fr",
             function() require('fzf-lua').resume() end,
             desc="Returns to FzF"
     },
-    { 
-            "<leader>fw", 
+    {
+            "<leader>fw",
             function() require('fzf-lua').grep_cword() end,
             desc="Find Current word"
     },
-    { 
-            "<leader><leader>", 
+    {
+            "<leader><leader>",
             function() require('fzf-lua').buffers() end,
-            desc="Find Existing Buffer "
+            desc="Find Existing Buffer"
+    },
+    {
+            "<leader>fo",
+            function() require('fzf-lua').oldfiles() end,
+            desc="Opens Recent Files/Oldfiles"
     },
   }
 }
