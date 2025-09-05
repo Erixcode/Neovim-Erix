82b8a8c4614695c8cd15867dbab6b6404655ea98
ErixPloit <35405767+Erixcode@users.noreply.github.com>
2025-09-05 17:01:37 +0330

    Add files via upload
    adding color

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 require("config.lazy")
 
---TreeView COonfiguration
+
+--TreeView Configurations: 
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- optionally enable 24-bit colour
 vim.opt.termguicolors = true
+vim.g.termguicolors = true
 
 -- empty setup using defaults
 require("nvim-tree").setup()
