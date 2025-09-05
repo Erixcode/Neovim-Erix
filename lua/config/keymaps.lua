0c4fea3c8dcdef449b932ec3716573c0cf7e0cb0
ErixPloit <35405767+Erixcode@users.noreply.github.com>
2025-09-05 17:03:23 +0330

    Add files via upload
    added colorizer leader+c keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,10 @@
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory in Oil" })
-vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, { desc = "Opens Diagnostic" })
+vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end,
+    { desc = "Opens Diagnostic" })
 vim.keymap.set("n", '<A-,>', '<Cmd>BufferPrevious<CR>', { desc = "Go to Previous Tab"})
 vim.keymap.set("n", '<A-.>', '<Cmd>BufferNext<CR>', { desc = "Go to Next Tab"})
 vim.keymap.set('n', '<A-p>', '<Cmd>BufferPin<CR>', { desc = "Pin Tab"})
 vim.keymap.set('n', '<A-c>', '<Cmd>BufferClose<CR>', { desc = "Close Tab"})
 vim.keymap.set('n', '<A-t>', '<Cmd>NvimTreeToggle<CR>', { desc = "Open/Close Treeview"})
 vim.keymap.set('n', '<leader>m', require('treesj').toggle , {desc = "JoinCode"})
+vim.keymap.set('n', '<leader>c', '<Cmd>ColorizerToggle<CR>', { desc = "Toggle Colorizer view"})
